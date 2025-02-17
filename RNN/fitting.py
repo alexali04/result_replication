7d4b9573d4f508d07a54bf5e34e99f1d660694af
Alex Ali <alexali000@gmail.com>
2025-02-17 16:31:39 -0500

    add path

--- RNN/fitting.py
+++ RNN/fitting.py
@@ -1,6 +1,7 @@
 import torch
 from rnn import RNN, RNNTrainer, RNNConfig, RNNTrainerConfig
 from pre_process import summarize_data, Tokenizer
+import argparse
 
 
 # constant
@@ -15,40 +16,45 @@ wandb_proj = "rnn"
 
 # try on small shakespeare dataset
 
-train_path = "./data/train.csv"
+def main(args):
+    train_path = args.train_path
 
-vocab_size, encoding_map, decoding_map = summarize_data(path=train_path, is_text=False)
+    vocab_size, encoding_map, decoding_map = summarize_data(path=train_path, is_text=False)
 
-tokenizer = Tokenizer(encoding_map=encoding_map, decoding_map=decoding_map)
+    tokenizer = Tokenizer(encoding_map=encoding_map, decoding_map=decoding_map)
 
-hello_world_loader = tokenizer.get_encoded_loader(train_path, batch_size=BSZ, is_text=False, seq_len=SEQ_LEN, step_size=10)
+    hello_world_loader = tokenizer.get_encoded_loader(train_path, batch_size=BSZ, is_text=False, seq_len=SEQ_LEN, step_size=10)
 
-rnn_config = RNNConfig(input_size=EMBD_DIM, hidden_size=HIDDEN_DIM, vocab_size=vocab_size, layer_count=LAYER_COUNT)
-model = RNN(rnn_config)
+    rnn_config = RNNConfig(input_size=EMBD_DIM, hidden_size=HIDDEN_DIM, vocab_size=vocab_size, layer_count=LAYER_COUNT)
+    model = RNN(rnn_config)
 
-optimizer = torch.optim.AdamW
-criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW
+    criterion = torch.nn.CrossEntropyLoss()
 
-rnn_trainer_config = RNNTrainerConfig(
-    model=model, 
-    train_loader=hello_world_loader, 
-    optimizer=optimizer, 
-    tokenizer=tokenizer, 
-    lr=lr, 
-    batch_size=BSZ, 
-    hidden_size=HIDDEN_DIM, 
-    wandb_project=wandb_proj
-)
-
-
-rnn_trainer = RNNTrainer(rnn_trainer_config)
-
-rnn_trainer.train(criterion)
+    rnn_trainer_config = RNNTrainerConfig(
+        model=model, 
+        train_loader=hello_world_loader, 
+        optimizer=optimizer, 
+        tokenizer=tokenizer, 
+        lr=lr, 
+        batch_size=BSZ, 
+        hidden_size=HIDDEN_DIM, 
+        wandb_project=wandb_proj
+    )
 
 
+    rnn_trainer = RNNTrainer(rnn_trainer_config)
 
+    rnn_trainer.train(criterion)
 
 
 
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--train_path", type=str, default="./data/train.csv")
+    args = args.parse_args()
+    main(args)
 
 
