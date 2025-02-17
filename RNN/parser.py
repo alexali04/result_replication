fd3ebdba7c01e6d80644f5ea0ec48b5150a9624f
Alex Ali <alexali000@gmail.com>
2025-02-17 16:53:52 -0500

    parsing

--- RNN/parser.py
+++ RNN/parser.py
@@ -6,6 +6,7 @@ def get_training_parser():
 
     parser.add_argument("--train_path", type=str, default="./data/train.csv")
     parser.add_argument("--wandb", type=argparse.BooleanOptionalAction)
+    parser.add_argument("--wandb_proj", type=str, default="rnn")
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--hidden_size", type=int, default=256)
     parser.add_argument("--layer_count", type=int, default=3)
