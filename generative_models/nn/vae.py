14ff9d607a8266f957d48e2abbb87e9cf9e5d1d2
alexali04 <alexali000@gmail.com>
2025-08-27 03:50:33 -0700

    Adding VAE Base

--- generative_models/nn/vae.py
+++ generative_models/nn/vae.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from result_replication.generative_models.nn.common import Activation_Dict
 
@@ -5,38 +6,41 @@ from result_replication.generative_models.nn.common import Activation_Dict
 
 
 class Encoder(nn.Module):
+    """Encoder that maps observable space to latent space - returns mu, sigma"""
     def __init__(self, 
         inp_dim, 
+        hidden_dim,
         latent_dim, 
         depth,
         activation
     ):
         super().__init__()
         self.inp_dim = inp_dim
-        self.latent_dim = latent_dim
+        self.hidden_dim = hidden_dim
         self.depth = depth
 
-        self.layer_1 = nn.Linear(self.inp_dim, self.latent_dim)
-        self.layer_2 = nn.Linear(self.latent_dim, self.latent_dim)
-        self.layer_3 = nn.Linear(self.latent_dim, self.latent_dim)
-        self.layer_4 = nn.Linear(self.latent_dim, self.latent_dim)
+        self.to_hidden = nn.Linear(self.inp_dim, self.hidden_dim)
+        self.hidden_layers = nn.ModuleList([nn.Linear(self.hidden_dim, self.hidden_dim) for _ in range(depth) - 1])
+        self.mu = nn.Linear(self.hidden_dim, self.hidden_dim)
+        self.sigma = nn.Linear(self.hidden_dim, self.hidden_dim)
 
-        self.to_latent = nn.Linear(self.inp_dim, self.latent_dim)
-
-        self.layers = nn.ModuleList([nn.Linear(self.latent_dim, self.latent_dim) for _ in range(depth) - 1])
         self.activation = Activation_Dict[activation]()
         
     def forward(self, x):
-        x_latent = self.to_latent(x)
+        x_hidden = self.to_hidden(x)
 
-        for layer in self.layers:
-            x_latent = layer(x_latent)
-            x_latent = self.activation(x_latent)
+        for layer in self.hidden_layers:
+            x_hidden = layer(x_hidden)
+            x_hidden = self.activation(x_hidden)
         
-        return x_latent
+        mu = self.mu(x_hidden)
+        sigma = self.sigma(x_hidden)
+
+        return mu, sigma
 
 
 class Decoder(nn.Module):
+    """Deterministic decoder that maps latents back to observable space"""
     def __init__(
         self,
         latent_dim,
@@ -75,16 +79,13 @@ class VAE(nn.Module):
         self.encoder = Encoder(inp_dim, latent_dim, depth, activation)
         self.decoder = Decoder(latent_dim, inp_dim, depth, activation)
 
-    def encode(self, x):
-        return self.encoder(x)
-
-    def decode(self, x):
-        return self.decoder(x)
-    
-
     def forward(self, x):
-        x_latent = self.encode(x)
-        x_out = self.decode(x_latent)
+        mu, sigma = self.encoder(x)
+        epsilon = torch.randn_like(mu)
 
-        return x_out
+        x_latent = mu + sigma * epsilon
+
+        x_hat = self.decoder(x_latent)
+
+        return x_hat
     
